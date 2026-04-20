1db2b11f96776e2a541b2b0945d92addce4bfdec
needa-khan <needak0205@gmail.com>
2026-04-21 00:42:51 +0530

    fix

--- app/(staff)/return/page.tsx
+++ app/(staff)/return/page.tsx
@@ -1,4 +1,6 @@
 "use client"
+export const dynamic = "force-dynamic";
+
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
